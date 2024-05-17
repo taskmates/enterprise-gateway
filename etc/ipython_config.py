8e893ee6c1a823701c8fc6ed9f9aab7db0c798ab
srizzo <rizzolabs@gmail.com>
2024-05-17 19:34:36 +0200

    cleanup

--- etc/ipython_config.py
+++ etc/ipython_config.py
@@ -3,11 +3,6 @@ c = get_config()
 ## -------------- CUSTOM BELOW --------------
 
 # Run all nodes interactively
-c.InteractiveShell.ast_node_interactivity = "all"
-
 c.InlineBackend.figure_format = 'retina'
-
 c.InteractiveShellApp.reraise_ipython_extension_failures = True
-# c.InteractiveShellApp.extensions = ['dask_extension']
-
 c.InteractiveShellApp.extensions = ['taskmates_enterprise_gateway.file_editing_magics']
